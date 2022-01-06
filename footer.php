ca2eac27f6d16fdc0d8bf837a6d9f940db2dee11
cammylim <s10196467@connect.np.edu.sg>
2022-01-06 22:41:34 +0800

    edited footer, header, index & mysql_conn

---
+++ footer.php
@@ -0,0 +1,14 @@
+            </div>
+        </div>
+        <!--4th Row-->
+        <div class="row">
+            <div class="col-sm-12" style="text-align:right;">
+            <hr/>
+            Do you need help? Please email to:
+            <a href="mailto:tastydonuts@np.edu.sg">tastydonuts@np.edu.sg</a>
+            <p style="font-size:12px">&copy;Copyright by Tasty Donuts</p>
+            </div>
+        </div>
+     </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 // Include the Page Layout header
 include("header.php"); 
 ?>
-<img src="Images/welcome2mamaya.jpg" class="img-fluid" 
+<img src="images/welcometotastydonuts.jpg" class="img-fluid" 
      style="display:block; margin:auto;"/>";
 
 <?php 
