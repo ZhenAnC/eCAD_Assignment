a53275081feb7a50f589f796aa43f0fa222d2f54
tan-jia-wen <s10196330@connect.np.edu.sg>
2022-02-07 02:15:13 +0800

    editing addMember.php

--- addMember.php
+++ addMember.php
@@ -3,6 +3,7 @@ session_start(); // Detect current session
 
 // Read data input from previous pages
 $name = $_POST["name"];
+$birthdate = $_POST["birthdate"];
 $address = $_POST["address"];
 $country = $_POST["country"];
 $phone = $_POST["phone"];
@@ -16,30 +17,48 @@ $answer = $_POST["answer"];
 // Include PHP file that establishes database connection handle: $conn
 include_once("mysql_conn.php");
 
-// Define INSERT SQL statement
-$qry = "INSERT INTO Shopper (Name, Address, Country, Phone, Email, Password, PwdQuestion, PwdAnswer)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-$stmt = $conn->prepare($qry);
-// "sss" - 3 string parameters
-$stmt->bind_param("ssssssss", $name, $address, $country, $phone, $email, $password, $security, $answer);
-
-if ($stmt->execute()) {
-    // Retrieve Shopper ID assigned to new shopper
-    $qry = "SELECT LAST_INSERT_ID() AS ShopperID";
-    $result = $conn->query($qry); // execute SQL and get returned result
-    while ($row = $result->fetch_array()) {
-        $_SESSION["ShopperID"] = $row["ShopperID"];
+$qrySearch = "SELECT Email FROM Shopper";
+$stmtSearch = $conn->prepare($qrySearch);
+$stmtSearch->bind_param("i");
+$stmtSearch->execute();
+$stmtSearch->close();
+$resSearch = $conn->query($qrySearch);
+while ($rowSearch = $resSearch->fetch_array()) {
+    if ($rowSearch[0] == $email) {
+        $emailCheck == true;
+        $message = "This email already exists!";
+        header("Location:register.php");
     }
+    else {
+        $emailCheck = false;
+        // Define INSERT SQL statement
+        $qry = "INSERT INTO Shopper (Name, BirthDate, Address, Country, Phone, Email, Password, PwdQuestion, PwdAnswer)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($qry);
+        // "sss" - 3 string parameters
+        $stmt->bind_param("sssssssss", $name, $birthdate, $address, $country, $phone, $email, $password, $security, $answer);
 
-    // Success message & shopper ID
-    $message = "Registration successful!<br />
-                Your ShopperID is $_SESSION[ShopperID]<br />";
-    // Save shopper name in session var
-    $_SESSION["ShopperName"] = $name;
+        if ($stmt->execute()) {
+            // Retrieve Shopper ID assigned to new shopper
+            $qry = "SELECT LAST_INSERT_ID() AS ShopperID";
+            $result = $conn->query($qry); // execute SQL and get returned result
+            while ($row = $result->fetch_array()) {
+                $_SESSION["ShopperID"] = $row["ShopperID"];
+            }
+
+            // Success message & shopper ID
+            $message = "Registration successful!<br />
+                    Your ShopperID is $_SESSION[ShopperID]<br />";
+            // Save shopper name in session var
+            $_SESSION["ShopperName"] = $name;
+        }
+        else { // Error message
+            $message = "<h3 style='color:red'>An error has occurred. Registration failed.</h3>";
+        }
+    }
 }
-else { // Error message
-    $message = "<h3 style='color:red'>An error has occurred. Registration failed.</h3>";
-}
+
+
 
 // Release resource allocated for prepared statement
 $stmt->close();
