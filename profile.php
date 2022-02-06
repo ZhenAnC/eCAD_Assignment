f0d6003c4e78e864828838af93e60db891170271
tan-jia-wen <s10196330@connect.np.edu.sg>
2022-02-07 00:37:40 +0800

    updated profile.php alignment

--- profile.php
+++ profile.php
@@ -31,36 +31,36 @@ while ($row = $result->fetch_array()) {
 <div style="width:80%; margin:auto;">
 <form name="editProfile" action="editProfileForm.php" method="post">
     <div class="form-group row">
-        <div class="col-sm-9 offset-sm-3">
+        <div class="col-sm-9">
             <span class="page-title">Profile</span>
         </div>
     </div>
     <div class="form-group row">
-        <label class="col-sm-3 col-form-label" for="name">Name:</label>
+        <label class="col-sm-3" for="name">Name:</label>
         <div class="col-sm-9">
             <span><?php echo $name; ?></span>
         </div>
     </div>
     <div class="form-group row">
-        <label class="col-sm-3 col-form-label" for="address">Address:</label>
+        <label class="col-sm-3" for="address">Address:</label>
         <div class="col-sm-9">
             <span><?php echo $address; ?></span>
         </div>
     </div>
     <div class="form-group row">
-        <label class="col-sm-3 col-form-label" for="country">Country:</label>
+        <label class="col-sm-3" for="country">Country:</label>
         <div class="col-sm-9">
             <span><?php echo $country; ?></span>
         </div>
     </div>
     <div class="form-group row">
-        <label class="col-sm-3 col-form-label" for="phone">Phone:</label>
+        <label class="col-sm-3" for="phone">Phone:</label>
         <div class="col-sm-9">
             <span><?php echo $phone; ?></span>
         </div>
     </div>
     <div class="form-group row">
-        <label class="col-sm-3 col-form-label" for="email">
+        <label class="col-sm-3" for="email">
             Email Address:</label>
         <div class="col-sm-9">
             <span><?php echo $email; ?></span>
